d6817a389c3dd3386a612fc5d6c0b54efe4261f2
Retr0IsNotAvailable <josephmagodks15846@gmail.com>
2025-11-23 13:00:02 +0000

    add QR generation and error handling animation functionalities

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,29 @@
 const inputElement = document.querySelector('.js-input');
 const buttonElement = document.querySelector('.js-button');
-const canvasElement = document.querySelector('.js-canvas');
+const imgElement = document.querySelector('.js-img');
 
 async function generateQR(inputValue = 'Example') {
-  const res = await fetch('https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Hello');
-  canvasElement.innerHTML = res;
-  canvasElement.style.display = 'block';
+  imgElement.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + inputValue;
+  imgElement.style.display = 'block';
+  inputElement.value = '';
 }
 
-generateQR();
+inputElement.addEventListener('keydown', e => {
+  if (e.key === 'Enter' && inputElement.value) {
+    generateQR(inputElement.value);
+  } else {
+    inputElement.classList.add('error');
+    
+    setTimeout(() => inputElement.classList.remove('error'), 1000);
+  }
+});
+
+buttonElement.addEventListener('click', () => {
+  if (inputElement.value) {
+    generateQR(inputElement.value);
+  } else {
+    inputElement.classList.add('error');
+
+    setTimeout(() => inputElement.classList.remove('error'), 1000);
+  }
+});
